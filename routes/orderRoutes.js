61bc08350217626745116194e467ccad65e865e1
Mai Mahmud <maimahmoudha99@gmail.com>
2025-05-10 01:51:59 +0300

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -4,13 +4,17 @@ const {
   addOrder,
   updateOrderStatus,
   deleteOrder,
+  getOrderById, 
+  getOrdersByDateRange,
 } = require("../controllers/orderController");
 
 const router = express.Router();
-
+router.get("/by-date", getOrdersByDateRange);
 router.get("/", getAllOrders);
 router.post("/", addOrder);
 router.patch("/:id", updateOrderStatus);
 router.delete("/:id", deleteOrder);
+router.get("/:id", getOrderById);
+
 
 module.exports = router;
