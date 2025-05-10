29ef974fad81c179260392348a0914c5be8df05a
Mai Mahmud <maimahmoudha99@gmail.com>
2025-05-10 16:45:28 +0300

    Update dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const dashboardController = require("../controllers/dashboardController");
 
 router.get("/summary", dashboardController.getSummary);
+//   GET Req --> http://localhost:3000/dashboard/summary
 
 module.exports = router;
